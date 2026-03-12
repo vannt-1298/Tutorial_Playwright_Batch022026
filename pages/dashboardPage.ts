b1d4e98d0e12b6fe997be25bc6921237c94a1129
Nguyễn Thị Vân <nguyen.thi.van@sun-asterisk.com>
2026-03-12 10:27:43 +0700

    Thêm mấy file thay đổi trong pages

--- pages/dashboardPage.ts
+++ pages/dashboardPage.ts
@@ -1,5 +1,25 @@
+import { Page, Locator } from '@playwright/test';
+
 export class DashboardPage {
+    private page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    getTitle(): Locator {
+        return this.page.locator('.title');
+    }
+
+    getFirstProduct(): Locator {
+        return this.page.locator('.inventory_item_name').first();
+    }
+
     async verifyLoginSuccess(): Promise<void> {
-        console.log("Verifying login success on Dashboard Page...");
+        await this.getTitle().isVisible();
+    }
+    async logout(): Promise<void> {
+        await this.page.click('#react-burger-menu-btn');
+        await this.page.click('#logout_sidebar_link');
     }
 }
--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,9 +1,40 @@
+import { Page, Locator } from '@playwright/test';
+
 export class LoginPage {
+    // 1. Khai báo thuộc tính Locator cho các phần tử trên trang Login
+    private page: Page;
+    private usernameInput: Locator;
+    private passwordInput: Locator;
+    private loginButton: Locator;
+    private errorMessage: Locator;
+
+    //2. Khởi tạo các constructor để gán giá trị cho các thuộc tính Locator
+    constructor(page: Page) {
+        this.page = page;
+        this.usernameInput = page.locator('#user-name');
+        this.passwordInput = page.locator('#password');
+        this.loginButton = page.locator('#login-button');
+        this.errorMessage = page.locator('[data-test="error"]');
+    }
+
+    // 3. Định nghĩa các phương thức để tương tác với các phần tử trên trang Login
     async gotoLoginPage(): Promise<void> {
-        console.log("Navigating to Login Page...");
+        await this.page.goto('https://www.saucedemo.com/', { waitUntil: 'domcontentloaded' });
     }
 
     async login(username: string, password: string): Promise<void> {
-        console.log(`Logging in with username: "${username}" and password: "${password}"`);
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+
+    // 4. Helper để lấy Locator của thông báo lỗi
+    getErrorMessage(): Locator {
+        return this.errorMessage;
+    }
+
+    // 5. Helper để kiểm tra lỗi hiển thị
+    async isErrorMessageVisible(): Promise<boolean> {
+        return await this.errorMessage.isVisible();
     }
 }
